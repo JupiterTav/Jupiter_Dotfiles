119d0f107c2007e2bd0efccacd4026b46096f5db
JupiterTav <wesleyTavaresDev@outlook.com>
2025-05-14 13:47:32 -0300

    initial files

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+	description = "A simple flake";
+
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+		home-manager = {
+			url = "github:nix-community/home-manager/release-24.11";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+};
+
+	outputs = {self, nixpkgs, home-manager}@inputs: {
+		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			modules = [
+				./configuration.nix
+				
+				home-manager.nixosModules.home-manager {
+					home-manager.useGlobalPkgs = true;
+					home-manager.useUserPackages = true;
+
+					home-manager.users.jupiter_euler = import ./home.nix;
+				}
+			];
+		};
+	};
+}
