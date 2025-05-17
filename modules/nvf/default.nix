cc669b9682a122f4ee1f89d0000bee071a193ed1
JupiterTav <wesleyTavaresDev@outlook.com>
2025-05-17 16:14:21 -0300

    nvf conf

--- modules/nvf/default.nix
+++ modules/nvf/default.nix
@@ -1,28 +1,61 @@
 {pkgs, lib, ...}: {
         
-        programs.nvf = {
-             enable =  true;
-	     settings = {
-                        vim = {
-                                theme = {
-                                        enable = true;
-                                        name = "gruvbox";
-                                        style = "dark";
-                                };
-
-                                statusline.lualine.enable = true;
-                                telescope.enable = true;
-                                autocomplete.nvim-cmp.enable = true;
-                                
-                                languages = {
-                                        enableLSP = true;
-                                        enableTreesitter = true;
-
-                                        nix.enable = true;
-                                        clang.enable = true;
-                                        rust.enable = true;
-                                };
-                        };
+programs.nvf = {
+    enable =  true;
+        settings = {
+            vim = {
+                options = {
+                    shiftwidth = 3;
+                    tabstop = 3;
                 };
+                
+                theme = {
+                    enable = true;
+                    name = "gruvbox";
+                    style = "dark";
+                };
+
+                statusline.lualine.enable = true;
+                autopairs.nvim-autopairs.enable = false;
+                telescope.enable = true;
+                autocomplete.blink-cmp.enable = true;
+                snippets.luasnip.enable = true;
+            
+                languages = {
+                    enableLSP = true;
+                    enableTreesitter = true;
+
+                    nix.enable = true;
+                    clang.enable = true;
+                    rust.enable = true;
+                };
+                lsp = {
+                    enable = true;
+                };
+                visuals = {
+                    nvim-web-devicons.enable = true;
+                    cinnamon-nvim.enable = true;
+                    indent-blankline.enable = true;
+                };
+
+                ui = {
+                    borders.enable = true;
+                    colorizer.enable = true;
+                    modes-nvim.enable = true;
+                };
+
+                binds = {
+                    whichKey.enable = true;
+                    cheatsheet.enable = true;
+                    hardtime-nvim.enable = true;
+                };
+
+                git = {
+                    enable = true;
+                    gitsigns.enable = true;
+                    gitsigns.codeActions.enable = false; 
+                };
+            };
         };
+    };
 }
